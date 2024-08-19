d71457b935a6e6527031356426490f0184ea1ecd
Tony Jin <tonyjzy@gmail.com>
2024-08-20 00:47:35 +1000

    add framer button example

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,11 +1,14 @@
+import { FramerButton } from "@/components/framer-button";
 import { MenuTap } from "@/components/menu-tap";
 import { RotateRect } from "@/components/rotate-rect";
 
 export default function Home() {
   return (
-    <main className="flex flex-col justify-center items-center gap-20">
+    <main className="flex flex-col justify-center items-center gap-[100px]">
       <div>Gooday Website</div>
       <RotateRect />
+
+      <FramerButton />
 
       <MenuTap />
     </main>
