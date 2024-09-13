dd05c0dbc0ac1fc7b4f2ce9f1450a351a7bfc9d4
maplesoup1 <91361133+maplesoup1@users.noreply.github.com>
2024-09-13 19:21:16 +1000

    character added to the animation

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -3,7 +3,6 @@ import Image from "next/image";
 import H1 from "../h1";
 import H2 from "../h2";
 import { Button } from "../ui/button";
-import { motion } from "framer-motion";
 
 const hero = () => {
   return (
@@ -40,19 +39,75 @@ const hero = () => {
       </div>
 
       {/* square */}
-      <div className="bg-gradient-to-b from-emerald-200 to-emerald-600 absolute w-[100px] h-[100px] left-[649px] top-[84px] rounded-md animate-updown_left"></div>
+      {/* character */}
+      <div className="bg-gradient-to-b from-emerald-200 to-emerald-600 absolute w-[100px] h-[100px] left-[649px] top-[84px] rounded-md  animate-updown_left hover:animate-none hover:scale-125 hover:rotate-[-13deg] cursor-pointer overflow-visible grid justify-center">
+        <Image
+          src="/half/Frank_half.png"
+          width={93}
+          height={93}
+          alt="charlotte"
+          className="relative bottom-[50px]"
+        />
+      </div>
+      <div className="bg-gradient-to-b from-green-200 to-green-600 absolute w-[100px] h-[100px] left-[1002px] top-[32px] rounded-md animate-updown_right hover:animate-none hover:scale-125 hover:rotate-[13deg] cursor-pointer overflow-visible grid justify-center">
+        <Image
+          src="/half/Olga_half.png"
+          width={80}
+          height={80}
+          alt="charlotte"
+          className="relative bottom-[59px]"
+        />
+      </div>
 
-      <div className="bg-gradient-to-b from-green-200 to-green-600 absolute w-[100px] h-[100px] left-[1002px] top-[32px] rounded-md animate-updown_right"></div>
+      <div className="bg-gradient-to-b from-yellow-200 to-yellow-600 absolute w-[100px] h-[100px] left-[566px] top-[480px] rounded-md animate-updown_right hover:animate-none hover:scale-125 hover:rotate-[13deg] cursor-pointer overflow-visible grid justify-center">
+        <Image
+          src="/half/Shutima_half.png"
+          width={100}
+          height={100}
+          alt="charlotte"
+          className="relative bottom-[28px] scale-125"
+        />
+      </div>
 
-      <div className="bg-gradient-to-b from-yellow-200 to-yellow-600 absolute w-[100px] h-[100px] left-[566px] top-[480px] rounded-md animate-updown_right"></div>
+      <div className="bg-gradient-to-b from-red-200 to-red-600 absolute w-[100px] h-[100px] left-[1250px] top-[63px] rounded-md animate-updown_right hover:animate-none hover:scale-125 hover:rotate-[13deg] cursor-pointer overflow-visible grid justify-center">
+        <Image
+          src="/half/Charlotte_half.png"
+          width={90}
+          height={90}
+          alt="charlotte"
+          className="relative bottom-12"
+        />
+      </div>
 
-      <div className="bg-gradient-to-b from-red-200 to-red-600 absolute w-[100px] h-[100px] left-[1250px] top-[63px] rounded-md animate-updown_right"></div>
+      <div className="bg-gradient-to-b from-orange-200 to-orange-600 absolute w-[100px] h-[100px] left-[1300px] top-[290px] rounded-md animate-updown_left hover:animate-none hover:scale-125 hover:rotate-[-13deg] cursor-pointer overflow-visible grid justify-center">
+        <Image
+          src="/half/Sinem_half.png"
+          width={100}
+          height={100}
+          alt="charlotte"
+          className="relative bottom-[34px] scale-125"
+        />
+      </div>
 
-      <div className="bg-gradient-to-b from-orange-200 to-orange-600 absolute w-[100px] h-[100px] left-[1300px] top-[290px] rounded-md animate-updown_left"></div>
+      <div className="bg-gradient-to-b from-purple-200 to-purple-600 absolute w-[100px] h-[100px] left-[1155px] top-[529px] rounded-md animate-updown_right hover:animate-none hover:scale-125 hover:rotate-[13deg] cursor-pointer overflow-visible grid justify-center">
+        <Image
+          src="/half/Raaj_half.png"
+          width={100}
+          height={100}
+          alt="charlotte"
+          className="relative bottom-[27px] scale-150"
+        />
+      </div>
 
-      <div className="bg-gradient-to-b from-purple-200 to-purple-600 absolute w-[100px] h-[100px] left-[1155px] top-[529px] rounded-md animate-updown_right"></div>
-
-      <div className="bg-gradient-to-b from-blue-200 to-blue-600 absolute w-[100px] h-[100px] left-[867px] top-[550px] rounded-md animate-updown_left"></div>
+      <div className="bg-gradient-to-b from-blue-200 to-blue-600 absolute w-[100px] h-[100px] left-[867px] top-[550px] rounded-md animate-updown_left hover:animate-none hover:scale-125 hover:rotate-[-13deg] cursor-pointer overflow-visible grid justify-center">
+        <Image
+          src="/half/Max_half.png"
+          width={90}
+          height={90}
+          alt="charlotte"
+          className="relative bottom-[28px] scale-150"
+        />
+      </div>
     </div>
   );
 };
