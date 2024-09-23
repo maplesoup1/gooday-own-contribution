b85a80b33418425ecf0a6c7f9d1507880946dbc4
maplesoup1 <timmy000728@gmail.com>
2024-09-23 23:53:23 +1000

    optimation

--- src/components/home/hero/CharacterGallery.tsx
+++ src/components/home/hero/CharacterGallery.tsx
@@ -1,136 +1,97 @@
 "use client";
 import React from "react";
 import Image from "next/image";
-
 interface CharacterButtonsProps {
   toggleCharacter: (character: string) => void;
 }
-
-interface CharacterInfo {
-  name: string;
-  gradientFrom: string;
-  gradientTo: string;
-  left: string;
-  top: string;
-  animation: string;
-  rotation: string;
-  imgWidth: number;
-  imgHeight: number;
-  imgBottom: string;
-  imgScale?: string;
-}
-
-const characters: CharacterInfo[] = [
-  {
-    name: "Frank",
-    gradientFrom: "emerald-200",
-    gradientTo: "emerald-600",
-    left: "580px",
-    top: "84px",
-    animation: "updown_left",
-    rotation: "-13deg",
-    imgWidth: 93,
-    imgHeight: 93,
-    imgBottom: "50px",
-  },
-  {
-    name: "Olga",
-    gradientFrom: "green-200",
-    gradientTo: "green-600",
-    left: "800px",
-    top: "60px",
-    animation: "updown_right",
-    rotation: "13deg",
-    imgWidth: 80,
-    imgHeight: 80,
-    imgBottom: "59px",
-  },
-  {
-    name: "Shutima",
-    gradientFrom: "yellow-200",
-    gradientTo: "yellow-600",
-    left: "480px",
-    top: "480px",
-    animation: "updown_right",
-    rotation: "13deg",
-    imgWidth: 100,
-    imgHeight: 100,
-    imgBottom: "28px",
-    imgScale: "scale-125",
-  },
-  {
-    name: "Charlotte",
-    gradientFrom: "red-200",
-    gradientTo: "red-600",
-    left: "970px",
-    top: "160px",
-    animation: "updown_right",
-    rotation: "13deg",
-    imgWidth: 90,
-    imgHeight: 90,
-    imgBottom: "12",
-  },
-  {
-    name: "Sinem",
-    gradientFrom: "orange-200",
-    gradientTo: "orange-600",
-    left: "980px",
-    top: "320px",
-    animation: "updown_left",
-    rotation: "-13deg",
-    imgWidth: 100,
-    imgHeight: 100,
-    imgBottom: "34px",
-    imgScale: "scale-125",
-  },
-  {
-    name: "Raaj",
-    gradientFrom: "purple-200",
-    gradientTo: "purple-600",
-    left: "900px",
-    top: "480px",
-    animation: "updown_right",
-    rotation: "13deg",
-    imgWidth: 100,
-    imgHeight: 100,
-    imgBottom: "27px",
-    imgScale: "scale-150",
-  },
-  {
-    name: "Max",
-    gradientFrom: "blue-200",
-    gradientTo: "blue-600",
-    left: "679px",
-    top: "550px",
-    animation: "updown_left",
-    rotation: "-13deg",
-    imgWidth: 90,
-    imgHeight: 90,
-    imgBottom: "28px",
-    imgScale: "scale-150",
-  },
-];
-
 const CharacterGallery: React.FC<CharacterButtonsProps> = ({ toggleCharacter }) => {
   return (
     <div>
-      {characters.map((char) => (
-        <div
-          key={char.name}
-          className={`z-50 bg-gradient-to-b from-${char.gradientFrom} to-${char.gradientTo} absolute w-[100px] h-[100px] left-[${char.left}] top-[${char.top}] rounded-md animate-${char.animation} hover:animate-none hover:scale-125 hover:rotate-[${char.rotation}] cursor-pointer overflow-visible grid justify-center`}
-          onClick={() => toggleCharacter(char.name)}
-        >
-          <Image
-            src={`/half/${char.name}_half.png`}
-            width={char.imgWidth}
-            height={char.imgHeight}
-            alt={char.name}
-            className={`relative ${char.name === "Charlotte" ? `bottom-${char.imgBottom}` : `bottom-[${char.imgBottom}]`} ${char.imgScale || ''}`}
-          />
-        </div>
-      ))}
+      <div
+        className="z-50 bg-gradient-to-b from-emerald-200 to-emerald-600 absolute w-[100px] h-[100px] left-[580px] top-[84px] rounded-md  animate-updown_left hover:animate-none hover:scale-125 hover:rotate-[-13deg] cursor-pointer overflow-visible grid justify-center"
+        onClick={() => toggleCharacter("Frank")}
+      >
+        <Image
+          src="/half/Frank_half.png"
+          width={93}
+          height={93}
+          alt="Frank"
+          className="relative bottom-[50px]"
+        />
+      </div>
+      <div
+        className="z-50 bg-gradient-to-b from-green-200 to-green-600 absolute w-[100px] h-[100px] left-[800px] top-[60px] rounded-md animate-updown_right hover:animate-none hover:scale-125 hover:rotate-[13deg] cursor-pointer overflow-visible grid justify-center"
+        onClick={() => toggleCharacter("Olga")}
+      >
+        <Image
+          src="/half/Olga_half.png"
+          width={80}
+          height={80}
+          alt="Olga"
+          className="relative bottom-[59px]"
+        />
+      </div>
+      <div
+        className="z-50 bg-gradient-to-b from-yellow-200 to-yellow-600 absolute w-[100px] h-[100px] left-[480px] top-[480px] rounded-md animate-updown_right hover:animate-none hover:scale-125 hover:rotate-[13deg] cursor-pointer overflow-visible grid justify-center"
+        onClick={() => toggleCharacter("Shutima")}
+      >
+        <Image
+          src="/half/Shutima_half.png"
+          width={100}
+          height={100}
+          alt="Shutima"
+          className="relative bottom-[28px] scale-125"
+        />
+      </div>
+      <div
+        className="z-50 bg-gradient-to-b from-red-200 to-red-600 absolute w-[100px] h-[100px] left-[970px] top-[160px] rounded-md animate-updown_right hover:animate-none hover:scale-125 hover:rotate-[13deg] cursor-pointer overflow-visible grid justify-center"
+        onClick={() => toggleCharacter("Charlotte")}
+      >
+        <Image
+          src="/half/Charlotte_half.png"
+          width={90}
+          height={90}
+          alt="Charlotte"
+          className="relative bottom-12"
+        />
+      </div>
+      <div
+        className="z-50 bg-gradient-to-b from-orange-200 to-orange-600 absolute w-[100px] h-[100px] left-[980px] top-[320px] rounded-md animate-updown_left hover:animate-none hover:scale-125 hover:rotate-[-13deg] cursor-pointer overflow-visible grid justify-center"
+        onClick={() => toggleCharacter("Sinem")}
+      >
+        <Image
+          src="/half/Sinem_half.png"
+          width={100}
+          height={100}
+          alt="Sinem"
+          className="relative bottom-[34px] scale-125"
+        />
+      </div>
+      <div
+        className="z-50 bg-gradient-to-b from-purple-200 to-purple-600 absolute w-[100px] h-[100px] left-[900px] top-[480px] rounded-md animate-updown_right hover:animate-none hover:scale-125 hover:rotate-[13deg] cursor-pointer overflow-visible grid justify-center"
+        onClick={() => toggleCharacter("Raaj")}
+      >
+        <Image
+          src="/half/Raaj_half.png"
+          width={100}
+          height={100}
+          alt="Raaj"
+          className="relative bottom-[27px] scale-150"
+        />
+      </div>
+      <div
+        className="z-50 bg-gradient-to-b from-blue-200 to-blue-600 absolute w-[100px] h-[100px] left-[679px] top-[550px] rounded-md animate-updown_left hover:animate-none hover:scale-125 hover:rotate-[-13deg] cursor-pointer overflow-visible grid justify-center"
+        onClick={() => toggleCharacter("Max")}
+      >
+        <Image
+          src="/half/Max_half.png"
+          width={90}
+          height={90}
+          alt="Max"
+          className="relative bottom-[28px] scale-150"
+        />
+      </div>
     </div>
   );
 };
-
 export default CharacterGallery;
